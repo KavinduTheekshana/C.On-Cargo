23882a8234271fda0a99646501933d792ea9ace0
Kavindu Theekshana <kavindutheekshana@gmail.com>
2023-11-23 22:48:10 +0000

    Login UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,173 @@
-<x-guest-layout>
+<!DOCTYPE html>
+
+<html lang="en" class="light-style layout-navbar-fixed layout-menu-fixed layout-compact" dir="ltr"
+    data-theme="theme-default" data-assets-path="{{ asset('/backend/assets') . '/' }}"
+    data-template="vertical-menu-template">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <title>C.ON Cargo - Your Trusted Cargo Partner</title>
+
+    <meta name="description" content="" />
+
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('frontend/assets/img/conlogoicon.svg') }}" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&ampdisplay=swap"
+        rel="stylesheet" />
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/fonts/materialdesignicons.css') }}" />
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/fonts/flag-icons.css') }}" />
+
+    <!-- Menu waves for no-customizer fix -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/libs/node-waves/node-waves.css') }}" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/css/rtl/core.css') }}"
+        class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/css/rtl/theme-default.css') }}"
+        class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/demo.css') }}" />
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/style.css') }}" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/libs/typeahead-js/typeahead.css') }}" />
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/libs/apex-charts/apex-charts.css') }}" />
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css') }}" />
+    <link rel="stylesheet"
+        href="{{ asset('backend/assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css') }}" />
+    <link rel="stylesheet"
+        href="{{ asset('backend/assets/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css') }}" />
+    <link rel="stylesheet"
+        href="{{ asset('backend/assets/vendor/libs/datatables-buttons-bs5/buttons.bootstrap5.css') }}" />
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/libs/select2/select2.css') }}" />
+    <link rel="stylesheet"
+        href="{{ asset('backend/assets/vendor/libs/@form-validation/umd/styles/index.min.css') }}" />
+
+    <!-- Page CSS -->
+
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/css/pages/app-logistics-dashboard.css') }}" />
+    @stack('styles')
+    <!-- Helpers -->
+    <script src="{{ asset('backend/assets/vendor/js/helpers.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/js/template-customizer.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/config.js') }}"></script>
+
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/css/pages/page-auth.css') }}" />
+</head>
+
+<body>
+    <!-- Content -->
+
+    <div class="authentication-wrapper authentication-cover">
+        <!-- Logo -->
+        <a href="{{ route('/') }}" class="auth-cover-brand d-flex align-items-center gap-2">
+
+            <img width="120px" src="{{ asset('frontend/assets/img/logo/logo.svg') }}" alt="" />
+
+
+        </a>
+        <!-- /Logo -->
+        <div class="authentication-inner row m-0">
+            <!-- /Left Section -->
+            <div class="d-none d-lg-flex col-lg-7 col-xl-8 align-items-center justify-content-center p-0">
+                <img style="width: 100%; height: 100%; object-fit: cover" src="{{ asset('frontend/assets/img/services/person.jpg')}}" alt="">
+
+            </div>
+            <!-- /Left Section -->
+
+            <!-- Login -->
+            <div
+                class="d-flex col-12 col-lg-5 col-xl-4 align-items-center authentication-bg position-relative py-sm-5 px-4 py-4">
+                <div class="w-px-400 mx-auto pt-5 pt-lg-0">
+                    <h4 class="mb-2">Welcome to C.ON Cargo! 👋</h4>
+                    <p class="mb-4">Please sign-in to your account and start the adventure</p>
+
+                    <form id="formAuthentication" class="mb-3" ction="{{ route('login') }}" method="POST">
+                        @csrf
+                        <div class="form-floating form-floating-outline mb-3">
+                            <input type="email" name="email" :value="old('email')" required autofocus
+                                autocomplete="username" class="form-control" id="email" name="email-username"
+                                placeholder="Enter your email or username" />
+                            <label for="email">Email or Username</label>
+                        </div>
+                        <div class="mb-3">
+                            <div class="form-password-toggle">
+                                <div class="input-group input-group-merge">
+                                    <div class="form-floating form-floating-outline">
+                                        <input type="password" id="password" class="form-control" name="password"
+                                            placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                            aria-describedby="password" required autocomplete="current-password"/>
+                                        <label for="password">Password</label>
+                                    </div>
+                                    <span class="input-group-text cursor-pointer"><i
+                                            class="mdi mdi-eye-off-outline"></i></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-3 d-flex justify-content-between">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="remember_me" name="remember" />
+                                <label class="form-check-label" for="remember-me"> Remember Me </label>
+                            </div>
+                            {{-- <a href="auth-forgot-password-cover.html" class="float-end mb-1">
+                  <span>Forgot Password?</span>
+                </a> --}}
+                        </div>
+                        <button type="submit" class="btn btn-primary d-grid w-100">Sign in</button>
+                    </form>
+
+
+
+
+
+                </div>
+            </div>
+            <!-- /Login -->
+        </div>
+    </div>
+
+    <!-- / Content -->
+
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="{{ asset('backend/assets/vendor/libs/jquery/jquery.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/libs/popper/popper.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/libs/node-waves/node-waves.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/libs/hammer/hammer.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/libs/i18n/i18n.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/libs/typeahead-js/typeahead.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/js/menu.js') }}"></script>
+
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+    <script src="{{ asset('backend/assets/vendor/libs/@form-validation/umd/bundle/popular.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/libs/@form-validation/umd/plugin-bootstrap5/index.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/libs/@form-validation/umd/plugin-auto-focus/index.min.js') }}"></script>
+
+    <!-- Main JS -->
+    <script src="{{ asset('backend/assets/js/main.js') }}"></script>
+
+    <!-- Page JS -->
+    <script src="{{ asset('backend/assets/js/pages-auth.js') }}"></script>
+</body>
+
+</html>
+
+
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -44,4 +213,4 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
